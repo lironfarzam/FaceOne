26f4d2a8d970798c946a1bddff228b048fdf2ee4
liron <lfarzam95@gmail.com>
2025-01-31 11:04:17 +0200

    clean the code

--- facenet512summary.py
+++
@@ -1,7 +0,0 @@
-# get the facenet512 model and print the model summary
-import tensorflow as tf
-from deepface import DeepFace
-
-model = DeepFace.build_model("Facenet512")
-keras_model = model.model
-keras_model.summary()
